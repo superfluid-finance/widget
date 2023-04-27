909b87179b6225f7e666a84ef98f3b4ceddc29ef
Kaspar Kallas <kaspar@superfluid.finance>
2023-04-28 01:01:05 +0300

    add command types

---
+++ packages/widget/src/commands.ts
@@ -0,0 +1,34 @@
+import { Address } from "abitype";
+import { ChainId, TimePeriod } from "superfluid-checkout-core";
+
+export type EnableAutoWrapCommand = {
+  title: "Enable Auto-Wrap";
+  chainId: ChainId;
+  superTokenAddress: Address;
+  underlyingTokenAddress: Address;
+};
+
+export type WrapIntoSuperTokensCommand = {
+  title: "Wrap into Super Tokens";
+  chainId: ChainId;
+  superTokenAddress: Address;
+  underlyingTokenAddress: Address;
+  amountEther: string;
+};
+
+export type SendStreamCommand = {
+  title: "Send Stream";
+  chainId: ChainId;
+  superTokenAddress: Address;
+  senderAddress: Address;
+  receiverAddress: Address;
+  flowRate: {
+    amountEther: string;
+    period: TimePeriod;
+  };
+};
+
+export type Command =
+  | EnableAutoWrapCommand
+  | WrapIntoSuperTokensCommand
+  | SendStreamCommand;
