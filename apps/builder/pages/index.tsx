2e52c9ce70dacd5069c498d2881dd4acf59c128b
tokdaniel <tokdaniel4@gmail.com>
2023-05-05 13:13:56 +0200

    widgetpreview initial

--- apps/builder/pages/index.tsx
+++ apps/builder/pages/index.tsx
@@ -1,32 +1,94 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
+import { useState } from "react";
+import { Stack } from "@mui/material";
+import { useTheme } from "@mui/material";
+import { Box } from "@mui/material";
+import WidgetPreview, {
+  WidgetProps,
+} from "@/components/widget-preview/WidgetPreview";
+import { Typography } from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
+import { NullableObject } from "@/types/general";
+import { networks } from "@/networkDefinitions";
+import tokenList from "@/tokenList";
+import { Select } from "@mui/material";
+import { MenuItem } from "@mui/material";
+import { SelectChangeEvent } from "@mui/material";
 
 export default function Home() {
+  const theme = useTheme();
+
+  const formMethods = useForm<NullableObject<WidgetProps>, any, WidgetProps>({
+    defaultValues: {
+      data: {
+        productName: "Product Name",
+        labels: {
+          from: "From",
+          to: "To",
+          network: "Select Network",
+          token: "SuperToken",
+          amount: "Amount",
+          send: "Send",
+        },
+        networks,
+        tokens: tokenList.tokens,
+      },
+      customStyle: {
+        root: {
+          width: 500,
+          px: 4,
+          py: 4,
+        },
+      },
+    },
+  });
+
+  const { watch, control, getValues } = formMethods;
+
+  const handleChange = (event: SelectChangeEvent<typeof selectedNetworks>) => {
+    const {
+      target: { value },
+    } = event;
+    setSelectedNetworks(typeof value === "string" ? value.split(",") : value);
+  };
+
   return (
-    <Container maxWidth="lg">
-      <Box
+    <Stack direction="row">
+      <Stack
         sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
+          width: 400,
+          height: "100vh",
         }}
       >
-        <Typography variant="h4" component="h1" gutterBottom>
-          Material UI - Next.js example in TypeScript
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
+        <Stack direction="column" p={2}>
+          <Typography variant="h6">Widget Customization</Typography>
+          <Stack direction="column">
+            <Controller
+              control={control}
+              name="data.networks"
+              render={() => (
+                <Select multiple value={selectedNetworks}>
+                  {networks.map((network) => (
+                    <MenuItem value={network.name} key={network.chainId}>
+                      {network.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              )}
+            />
+          </Stack>
+        </Stack>
+      </Stack>
+      <Box
+        sx={{
+          width: "100%",
+          backgroundColor: theme.palette.grey[900],
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <WidgetPreview {...getValues()} />
       </Box>
-    </Container>
+    </Stack>
   );
 }
