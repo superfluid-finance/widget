cb98b4d988018235d654644d90784f31eec9f028
Kaspar Kallas <kaspar@superfluid.finance>
2023-05-05 12:56:06 +0300

    use resetField over setValue

--- packages/widget/src/FormEffects.tsx
+++ packages/widget/src/FormEffects.tsx
@@ -3,7 +3,7 @@ import { DraftFormValues } from "./formValues";
 import { useEffect } from "react";
 
 export function FormEffects() {
-  const { watch, setValue } = useFormContext<DraftFormValues>();
+  const { watch, resetField } = useFormContext<DraftFormValues>();
 
   const [network, paymentOptionWithTokenInfo] = watch([
     "network",
@@ -12,19 +12,24 @@ export function FormEffects() {
 
   // Reset payment option (i.e. the token) when network changes.
   useEffect(() => {
-    setValue("paymentOptionWithTokenInfo", null, {
-      shouldValidate: true,
-      shouldDirty: true,
-      shouldTouch: false,
+    resetField("paymentOptionWithTokenInfo", {
+      keepDirty: true,
+      keepTouched: true,
+      keepError: true,
     });
   }, [network]);
 
   // Reset wrap amount when payment option (i.e. the token) changes.
   useEffect(() => {
-    setValue("wrapAmountEther", "", {
-      shouldValidate: true,
-      shouldDirty: true,
-      shouldTouch: false,
+    resetField("wrapAmountEther", {
+      keepDirty: true,
+      keepTouched: true,
+      keepError: true,
+    });
+    resetField("enableAutoWrap", {
+      keepDirty: true,
+      keepTouched: true,
+      keepError: true,
     });
   }, [paymentOptionWithTokenInfo]);
 
