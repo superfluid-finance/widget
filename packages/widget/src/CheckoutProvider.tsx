5bcfaace557648894e56ae483186dc8d3deb13d8
Kaspar Kallas <kaspar@superfluid.finance>
2023-05-09 15:20:43 +0300

    use widget inside the builder (fixed)

--- packages/widget/src/CheckoutProvider.tsx
+++ packages/widget/src/CheckoutProvider.tsx
@@ -26,7 +26,11 @@ export function CheckoutProvider({
   // ---
 
   const supportedNetworks_ = useMemo<ReadonlyArray<SupportedNetwork>>(() => {
-    const uniqueChainIds = [...new Set(paymentOptions.map((x) => x.chainId))];
+    // TODO: In some cases, produces this error: Type 'Set<5 | 80001 | 420 | 421613 | 43113 | 100 | 137 | 10 | 42161 | 43114 | 56 | 1 | 42220>' can only be iterated through when using the '--downlevelIteration' flag or with a '--target' of 'es2015' or higher.
+    // const uniqueChainIds = [...new Set(paymentOptions.map((x) => x.chainId))];
+    const uniqueChainIds = paymentOptions
+      .map((x) => x.chainId)
+      .filter((chainId, index, self) => self.indexOf(chainId) === index);
     return uniqueChainIds
       .map((chainId) => {
         const supportedNetwork = supportedNetworks.find(
@@ -41,7 +45,7 @@ export function CheckoutProvider({
         return supportedNetwork;
       })
       .filter((x): x is SupportedNetwork => x !== null);
-  }, [paymentOptions]); 
+  }, [paymentOptions]);
 
   // Derive autocomplete options from the payment options.
   const paymentOptionWithTokenInfoList = useMemo<
