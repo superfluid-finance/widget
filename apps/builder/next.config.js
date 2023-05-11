6727efa88e9fec121734701ba8b18cf365ad4d8b
tokdaniel <tokdaniel4@gmail.com>
2023-05-11 14:40:55 +0200

    fix module not found next errors

--- apps/builder/next.config.js
+++ apps/builder/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    transpilePackages: ["superfluid-checkout-widget"]
+  transpilePackages: ["superfluid-checkout-widget"],
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+
+    return config;
+  },
 };
