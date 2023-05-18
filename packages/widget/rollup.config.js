64aee2a102d71096a849fd6dbf7b9619fb700063
tokdaniel <tokdaniel4@gmail.com>
2023-05-18 18:20:04 +0200

    remove rollup stuff as its unnecessary

--- packages/widget/rollup.config.js
+++
@@ -1,9 +0,0 @@
-import terser from '@rollup/plugin-terser'
-import multiEntry from "@rollup/plugin-multi-entry"
-export default {
-	input: ['dist/src/index.js', 'dist/src/WebComponent.js'],
-	output: {
-		file: 'dist/index.js',
-	},
-    plugins: [terser(), multiEntry()]
-};
