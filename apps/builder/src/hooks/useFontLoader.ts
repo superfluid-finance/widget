18eeb3dc708b5c0bc99d765d3344f9cf7f5c66f4
tokdaniel <tokdaniel4@gmail.com>
2023-05-26 15:58:43 +0200

    dont load fonts if theres nothing chose

--- apps/builder/src/hooks/useFontLoader.ts
+++ apps/builder/src/hooks/useFontLoader.ts
@@ -3,6 +3,8 @@ import { DisplaySettings } from "../components/widget-preview/WidgetPreview";
 
 const useFontLoader = (displaySettings: DisplaySettings) => {
   useEffect(() => {
+    if (displaySettings.font.config?.family === undefined) return;
+
     const link = document.createElement("link");
     link.rel = "stylesheet";
     link.href = `https://fonts.googleapis.com/css2?family=${displaySettings.font.config?.family}:wght@400;500&display=swap`;
