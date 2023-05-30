cf5e8336894a9900a59c07732c38ede8f0f50c0a
Kaspar Kallas <kaspar@superfluid.finance>
2023-05-30 15:50:39 +0300

    clean up hosted build

--- apps/hosted-widget/next.config.js
+++ apps/hosted-widget/next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
