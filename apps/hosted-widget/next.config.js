dfe0b20bcd877ca9144926208a246d319ce11171
Kaspar Kallas <kaspar@superfluid.finance>
2023-05-31 14:01:40 +0300

    remove static exports

--- apps/hosted-widget/next.config.js
+++ apps/hosted-widget/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  images: {
-    unoptimized: true,
-  },
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
