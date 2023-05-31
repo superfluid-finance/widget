c880306b677e865615e53d7fc87c76f8d3a12e82
Mikk <mikk@superfluid.finance>
2023-05-31 17:00:58 +0300

    Improved avatar icon

--- packages/widget/src/NetworkAvatar.tsx
+++ packages/widget/src/NetworkAvatar.tsx
@@ -1,37 +1,109 @@
-import { Avatar } from "@mui/material";
+import { Avatar, AvatarProps } from "@mui/material";
 import { FC, useMemo } from "react";
 import { SupportedNetwork } from "./core";
 
+// TODO: (MÕ) Merge this conf with supported networks
+export const networkAssetsConf = [
+  {
+    name: "Goerli",
+    chainId: 5,
+    color: "#9064ff",
+  },
+  {
+    name: "Polygon Mumbai",
+    chainId: 80001,
+    color: "#3099f2",
+  },
+  {
+    name: "Optimism Goerli",
+    chainId: 420,
+    color: "#ff0320",
+  },
+  {
+    name: "Arbitrum Goerli",
+    chainId: 421613,
+    color: "#2b374b",
+  },
+  {
+    name: "Avalanche Fuji",
+    chainId: 43113,
+    color: "#2b374b",
+  },
+  {
+    name: "Gnosis Chain",
+    chainId: 100,
+    logoUrl: `https://raw.githubusercontent.com/superfluid-finance/widget/master/apps/widget-builder/public/assets/network-icons/gnosis.svg`,
+    color: "#04795b",
+  },
+  {
+    name: "Polygon",
+    chainId: 137,
+    logoUrl: `https://raw.githubusercontent.com/superfluid-finance/widget/master/apps/widget-builder/public/assets/network-icons/polygon.svg`,
+    color: "#7c3fe4",
+  },
+  {
+    name: "Optimism",
+    chainId: 10,
+    logoUrl: `https://raw.githubusercontent.com/superfluid-finance/widget/master/apps/widget-builder/public/assets/network-icons/optimism.svg`,
+    color: "#ff0320",
+  },
+  {
+    name: "Arbitrum One",
+    chainId: 42161,
+    logoUrl: `https://raw.githubusercontent.com/superfluid-finance/widget/master/apps/widget-builder/public/assets/network-icons/arbitrum.svg`,
+    color: "#2b374b",
+  },
+  {
+    name: "Avalanche C",
+    chainId: 43114,
+    logoUrl: `https://raw.githubusercontent.com/superfluid-finance/widget/master/apps/widget-builder/public/assets/network-icons/avalanche.svg`,
+    color: "#e84142",
+  },
+  {
+    name: "BNB Smart Chain",
+    chainId: 56,
+    logoUrl: `https://raw.githubusercontent.com/superfluid-finance/widget/master/apps/widget-builder/public/assets/network-icons/bnb.svg`,
+    color: "#F0B90B",
+  },
+  {
+    name: "Ethereum",
+    chainId: 1,
+    logoUrl: `https://raw.githubusercontent.com/superfluid-finance/widget/master/apps/widget-builder/public/assets/network-icons/ethereum.svg`,
+    color: "#627EEA",
+  },
+  {
+    name: "Celo",
+    chainId: 42220,
+    logoUrl: `https://raw.githubusercontent.com/superfluid-finance/widget/master/apps/widget-builder/public/assets/network-icons/celo.svg`,
+    color: "#FCFF52",
+  },
+];
+
 interface NetworkAvatarProps {
   network: SupportedNetwork;
+  AvatarProps?: Partial<AvatarProps>;
 }
 
-const networkIconNames = {
-  42161: "arbitrum",
-  43114: "avalanche",
-  56: "binance",
-  42220: "celo",
-  1: "ethereum",
-  100: "gnosis",
-  10: "optimism",
-  137: "polygon",
-};
-
-const NetworkAvatar: FC<NetworkAvatarProps> = ({ network }) => {
-  const networkIconUrl = useMemo(() => {
-    if (network.testnet) return undefined;
-
-    const iconName = networkIconNames[network.id];
-    if (!iconName) return undefined;
-
-    return `https://raw.githubusercontent.com/superfluid-finance/widget/master/apps/widget-builder/public/assets/network-icons/${iconName}.svg`;
+const NetworkAvatar: FC<NetworkAvatarProps> = ({
+  network,
+  AvatarProps = {},
+}) => {
+  const networkAssetConf = useMemo(() => {
+    return networkAssetsConf.find((conf) => conf.chainId === network.id);
   }, [network]);
+
+  const { sx: AvatarSx = {} } = AvatarProps;
 
   return (
     <Avatar
-      src={networkIconUrl}
+      src={networkAssetConf?.logoUrl}
       variant="rounded"
-      sx={{ objectFit: "contain" }}
+      {...AvatarProps}
+      sx={{
+        objectFit: "contain",
+        backgroundColor: networkAssetConf?.color,
+        ...AvatarSx,
+      }}
     >
       {network.testnet && network.name.charAt(0)}
     </Avatar>
