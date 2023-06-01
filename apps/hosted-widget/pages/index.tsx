5ec381ff0b7a5afc200357b65fade434dfa498ff
tokdaniel <tokdaniel4@gmail.com>
2023-06-01 16:01:26 +0200

    redirect to builder if no ipfs hash

--- apps/hosted-widget/pages/index.tsx
+++ apps/hosted-widget/pages/index.tsx
@@ -1,23 +1,7 @@
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        Something
-      </Box>
-    </Container>
-  );
+  return null;
 };
 
 export default Home;
