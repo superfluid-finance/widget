9a192552e4fe5f9d30eeced19122facc261859d8
Kaspar Kallas <kaspar@superfluid.finance>
2023-06-30 14:15:39 +0300

    clean up code a bit

--- apps/widget-builder/src/pages/_app.tsx
+++ apps/widget-builder/src/pages/_app.tsx
@@ -25,28 +25,32 @@ const projectId =
   process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ??
   "952483bf7a0f5ace4c40eb53967f1368";
 
+const {
+  arbitrum,
+  avalanche,
+  avalancheFuji,
+  bsc,
+  celo,
+  goerli,
+  gnosis,
+  mainnet,
+  optimism,
+  polygon,
+  polygonMumbai,
+} = supportedNetwork;
+
 export const superfluidRpcUrls = {
-  [supportedNetwork.arbitrum.id]:
-    "https://rpc-endpoints.superfluid.dev/arbitrum-one",
-  [supportedNetwork.avalanche.id]:
-    "https://rpc-endpoints.superfluid.dev/avalanche-c",
-  [supportedNetwork.avalancheFuji.id]:
-    "https://rpc-endpoints.superfluid.dev/avalanche-fuji",
-  [supportedNetwork.bsc.id]: "https://rpc-endpoints.superfluid.dev/bsc-mainnet",
-  [supportedNetwork.celo.id]:
-    "https://rpc-endpoints.superfluid.dev/celo-mainnet",
-  [supportedNetwork.goerli.id]:
-    "https://rpc-endpoints.superfluid.dev/eth-goerli",
-  [supportedNetwork.gnosis.id]:
-    "https://rpc-endpoints.superfluid.dev/xdai-mainnet",
-  [supportedNetwork.mainnet.id]:
-    "https://rpc-endpoints.superfluid.dev/eth-mainnet",
-  [supportedNetwork.optimism.id]:
-    "https://rpc-endpoints.superfluid.dev/optimism-mainnet",
-  [supportedNetwork.polygon.id]:
-    "https://rpc-endpoints.superfluid.dev/polygon-mainnet",
-  [supportedNetwork.polygonMumbai.id]:
-    "https://rpc-endpoints.superfluid.dev/polygon-mumbai",
+  [arbitrum.id]: "https://rpc-endpoints.superfluid.dev/arbitrum-one",
+  [avalanche.id]: "https://rpc-endpoints.superfluid.dev/avalanche-c",
+  [avalancheFuji.id]: "https://rpc-endpoints.superfluid.dev/avalanche-fuji",
+  [bsc.id]: "https://rpc-endpoints.superfluid.dev/bsc-mainnet",
+  [celo.id]: "https://rpc-endpoints.superfluid.dev/celo-mainnet",
+  [goerli.id]: "https://rpc-endpoints.superfluid.dev/eth-goerli",
+  [gnosis.id]: "https://rpc-endpoints.superfluid.dev/xdai-mainnet",
+  [mainnet.id]: "https://rpc-endpoints.superfluid.dev/eth-mainnet",
+  [optimism.id]: "https://rpc-endpoints.superfluid.dev/optimism-mainnet",
+  [polygon.id]: "https://rpc-endpoints.superfluid.dev/polygon-mainnet",
+  [polygonMumbai.id]: "https://rpc-endpoints.superfluid.dev/polygon-mumbai",
 } as const;
 
 const { chains, publicClient } = configureChains(supportedNetworks, [
