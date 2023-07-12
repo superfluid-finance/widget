49cbcc01197303a38fc8002859f9b111b7c09419
Mikk <mikk@superfluid.finance>
2023-07-12 12:28:21 +0300

    Responsiveness improvements

--- examples/b2b-service-demo/src/components/Footer/Footer.tsx
+++ examples/b2b-service-demo/src/components/Footer/Footer.tsx
@@ -7,33 +7,35 @@ interface FooterProps {}
 const Footer: FC<FooterProps> = ({}) => {
   return (
     <footer className={styles.Wrapper}>
-      <div className={styles.Intro}>
-        <img src="/logo.svg" alt="Logo" />
-        <p>The web3 development platform</p>
-        <div className={styles.Social}>
-          <img src="/social/facebook.svg" alt="Facebook button" />
-          <img src="/social/instagram.svg" alt="Instagram button" />
-          <img src="/social/twitter.svg" alt="Twitter icon" />
+      <div className={styles.Content}>
+        <div className={styles.Intro}>
+          <img className={styles.Logo} src="/logo.svg" alt="Logo" />
+          <p>The web3 development platform</p>
+          <div className={styles.Social}>
+            <img src="/social/facebook.svg" alt="Facebook button" />
+            <img src="/social/instagram.svg" alt="Instagram button" />
+            <img src="/social/twitter.svg" alt="Twitter icon" />
+          </div>
         </div>
-      </div>
-      <div className={styles.Links}>
-        <FooterSection
-          title="Products"
-          links={["API", "SDK", "Supernode", "Webhooks"]}
-        />
-        <FooterSection
-          title="For Developers"
-          links={["Faucet", "Smart Contracts", "Docs"]}
-        />
-        <FooterSection
-          title="Company"
-          links={[
-            "About us",
-            "Privacy Policy",
-            "Terms & Conditions",
-            "Contact Us",
-          ]}
-        />
+        <div className={styles.Links}>
+          <FooterSection
+            title="Products"
+            links={["API", "SDK", "Supernode", "Webhooks"]}
+          />
+          <FooterSection
+            title="For Developers"
+            links={["Faucet", "Smart Contracts", "Docs"]}
+          />
+          <FooterSection
+            title="Company"
+            links={[
+              "About us",
+              "Privacy Policy",
+              "Terms & Conditions",
+              "Contact Us",
+            ]}
+          />
+        </div>
       </div>
     </footer>
   );
--- examples/b2b-service-demo/src/pages/index.tsx
+++ examples/b2b-service-demo/src/pages/index.tsx
@@ -26,64 +26,63 @@ export default function Home() {
           <img src="/logo.svg" alt="Logo" />
         </header>
 
-        <div className={styles.Wrapper}>
-          <section>
-            <div className={styles.Heading}>
-              <h1>Choose a pricing plan</h1>
-              <p>Build and scale your app easier than ever before.</p>
-            </div>
-            <div className={styles.PricingOptions}>
-              <PricingCard
-                title="Starter"
-                description="Up to 1,000,000 compute units/mo"
-                ctaTitle="Subscribe"
-                price={29}
-                onFinish={openModal}
-                features={[
-                  "Supernode, Build, Monitor, and Notify",
-                  "Enhanced APIs",
-                  "Multichain mainnets and testnets",
-                  "No daily request limits",
-                  "Full archive data",
-                ]}
-              />
+        <section className={styles.Wrapper}>
+          <div className={styles.Heading}>
+            <h1>Choose a pricing plan</h1>
+            <p>Build and scale your app easier than ever before.</p>
+          </div>
+          <div className={styles.PricingOptions}>
+            <PricingCard
+              title="Starter"
+              description="Up to 1,000,000 compute units/mo"
+              ctaTitle="Subscribe"
+              price={29}
+              onFinish={openModal}
+              features={[
+                "Supernode, Build, Monitor, and Notify",
+                "Enhanced APIs",
+                "Multichain mainnets and testnets",
+                "No daily request limits",
+                "Full archive data",
+              ]}
+            />
 
-              <PricingCard
-                title="Growth"
-                description="Up to 5,000,000 compute units/mo"
-                ctaTitle="Subscribe"
-                price={99}
-                onFinish={openModal}
-                features={[
-                  "Auto-scaling compute units",
-                  "Parity trace and gETH debug",
-                  "Access to reinforced transactions",
-                  "Early access t2x higher throughput",
-                  "Access to Paymaster Services",
-                ]}
-              />
+            <PricingCard
+              title="Growth"
+              description="Up to 5,000,000 compute units/mo"
+              ctaTitle="Subscribe"
+              price={99}
+              onFinish={openModal}
+              features={[
+                "Auto-scaling compute units",
+                "Parity trace and gETH debug",
+                "Access to reinforced transactions",
+                "Early access t2x higher throughput",
+                "Access to Paymaster Services",
+              ]}
+            />
 
-              <PricingCard
-                outlined
-                title="Business"
-                description="Custom compute units"
-                ctaTitle="Talk to us"
-                price={1000}
-                onClick={openModal}
-                features={[
-                  "Uncapped Simulation Throughput",
-                  "Committed use discounts",
-                  "Custom SLAs",
-                  "Custom throughput",
-                  "24/7 VIP support",
-                ]}
-              />
-            </div>
-          </section>
+            <PricingCard
+              outlined
+              title="Business"
+              description="Custom compute units"
+              ctaTitle="Talk to us"
+              price={1000}
+              onClick={openModal}
+              features={[
+                "Uncapped Simulation Throughput",
+                "Committed use discounts",
+                "Custom SLAs",
+                "Custom throughput",
+                "24/7 VIP support",
+              ]}
+            />
+          </div>
+        </section>
 
-          <BookModal show={showModal} onClose={closeModal} />
-          <Footer />
-        </div>
+        <BookModal show={showModal} onClose={closeModal} />
+
+        <Footer />
       </main>
     </>
   );
