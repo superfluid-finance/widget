67a6257fdb83fc52cec0410bb8d7da1ab7885742
tokdaniel <tokdaniel4@gmail.com>
2023-08-04 12:31:54 +0200

    remove Web3Modal from demo-providers as its unnecessary

--- apps/hosted-widget/src/demo-providers.tsx
+++ apps/hosted-widget/src/demo-providers.tsx
@@ -1,12 +1,7 @@
-import { EthereumClient } from "@web3modal/ethereum";
-import { Web3Modal } from "@web3modal/react";
 import * as React from "react";
 import { WagmiConfig } from "wagmi";
 
-import { walletConnectProjectId } from "./wagmi";
-import { wagmiChainsDemo, wagmiConfigDemo } from "./wagmi-demo";
-
-const ethereumClient = new EthereumClient(wagmiConfigDemo, wagmiChainsDemo);
+import { wagmiConfigDemo } from "./wagmi-demo";
 
 export function WagmiDemoProviders({
   children,
@@ -16,12 +11,6 @@ export function WagmiDemoProviders({
   const [mounted, setMounted] = React.useState(false);
   React.useEffect(() => setMounted(true), []);
   return (
-    <WagmiConfig config={wagmiConfigDemo}>
-      {mounted && children}
-      <Web3Modal
-        projectId={walletConnectProjectId}
-        ethereumClient={ethereumClient}
-      />
-    </WagmiConfig>
+    <WagmiConfig config={wagmiConfigDemo}>{mounted && children}</WagmiConfig>
   );
 }
