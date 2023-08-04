a9d6248758108c55f7de47d7e6a611ac1af50b4b
tokdaniel <tokdaniel4@gmail.com>
2023-08-04 13:38:29 +0200

    captalize DEMO in demo related files

--- apps/hosted-widget/src/demo-providers.tsx
+++ apps/hosted-widget/src/demo-providers.tsx
@@ -7,7 +7,7 @@
 import * as React from "react";
 import { WagmiConfig } from "wagmi";
 
-import { wagmiConfigDemo } from "./wagmi-demo";
+import { wagmiConfigDemo } from "./DEMO-wagmi";
 
 export function WagmiDemoProviders({
   children,
