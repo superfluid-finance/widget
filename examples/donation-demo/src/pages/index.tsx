e3c796b0177cbd2f95cbd8f0d142e1db45c7aa11
Kaspar Kallas <kaspar@superfluid.finance>
2023-08-14 12:50:39 +0000

    linting

--- examples/donation-demo/src/pages/index.tsx
+++ examples/donation-demo/src/pages/index.tsx
@@ -1,4 +1,3 @@
-
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import ProjectCard from "@/components/ProjectCard/ProjectCard";
--- examples/donation-demo/src/pages/project.tsx
+++ examples/donation-demo/src/pages/project.tsx
@@ -1,4 +1,3 @@
-
 import Content from "@/components/Content/Content";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
