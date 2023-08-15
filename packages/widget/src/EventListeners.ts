c901e1f35e245b5c1e43bccbd3a402c8de4b707a
Kaspar Kallas <kaspar@superfluid.finance>
2023-08-15 13:02:10 +0000

    update comment
    --skip-ci

--- packages/widget/src/EventListeners.ts
+++ packages/widget/src/EventListeners.ts
@@ -6,6 +6,7 @@ import { PaymentOption } from "./core";
  * <SuperfluidWidget eventListeners={{
  *   onSuccess: () => console.log('Checkout is successfully finished!'),
  *   onSuccessButtonClick: () => console.log('Merchant success button is clicked!')
+ *   onPaymentOptionUpdate: (paymentOption) => setChainId(paymentOption?.chainId);
  * }} />
  */
 export interface EventListeners {
@@ -14,7 +15,7 @@ export interface EventListeners {
   /** Called when the merchant's success button is defined in the schema and it's clicked. */
   onSuccessButtonClick?: () => void;
   /**
-   *
+   * Called when the payment option is initialized or changed by the user.
    */
   onPaymentOptionUpdate?: (paymentOption?: PaymentOption) => void;
 }
