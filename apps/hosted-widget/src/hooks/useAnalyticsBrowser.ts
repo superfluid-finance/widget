4bc9dacef7b029e56df217ce1d0088c706425988
Kaspar Kallas <kaspar@superfluid.finance>
2023-09-13 14:23:53 +0000

    use singleton for analytics browser

--- apps/hosted-widget/src/hooks/useAnalyticsBrowser.ts
+++ apps/hosted-widget/src/hooks/useAnalyticsBrowser.ts
@@ -4,20 +4,33 @@ import { useMemo } from "react";
 export const SSR = typeof window === "undefined";
 export const IsCypress = !SSR && !!(window as any).Cypress; // https://docs.cypress.io/faq/questions/using-cypress-faq#Is-there-any-way-to-detect-if-my-app-is-running-under-Cypress
 
-const useAnalyticsBrowser = () =>
-  useMemo(() => {
-    const writeKey = process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY;
-    if (!IsCypress && writeKey) {
-      return AnalyticsBrowser.load(
-        { writeKey },
-        {
-          initialPageview: true,
-        },
-      );
-    } else {
-      console.warn("Segment not initialized. No-op instance provided instead.");
-      return AnalyticsBrowser.load({ writeKey: "NOOP" });
+class AnalyticsBrowserSingleton {
+  private static instance: AnalyticsBrowser | null = null;
+
+  public static getInstance(writeKey: string | undefined): AnalyticsBrowser {
+    if (!this.instance) {
+      if (!IsCypress && writeKey) {
+        this.instance = AnalyticsBrowser.load(
+          { writeKey },
+          { initialPageview: true },
+        );
+      } else {
+        console.warn(
+          "Segment not initialized. No-op instance provided instead.",
+        );
+        this.instance = AnalyticsBrowser.load({ writeKey: "NOOP" });
+      }
     }
-  }, []);
+    return this.instance;
+  }
+}
+
+export const useAnalyticsBrowser = () => {
+  const writeKey = process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY;
+  return useMemo(
+    () => AnalyticsBrowserSingleton.getInstance(writeKey),
+    [writeKey],
+  );
+};
 
 export default useAnalyticsBrowser;
