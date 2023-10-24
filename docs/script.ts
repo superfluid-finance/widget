aeb7b2e3590d2965154caddbfb47fdb485e915e2
Kaspar Kallas <kaspar@superfluid.finance>
2023-10-24 13:34:40 +0000

    no-op for ci

--- docs/script.ts
+++ docs/script.ts
@@ -3,7 +3,7 @@ import { createDocumentation } from "typedoc-nextra";
 await createDocumentation({
   // use existing typedoc json output (leave it blank to auto generate)
   jsonInputPath: `${__dirname}/data.json`,
-  // output location
+  // output  location
   output: `${__dirname}/pages/api`,
   // output markdown
   markdown: true,
