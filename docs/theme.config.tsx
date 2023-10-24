bcc1cfe50bf3b2f365f254ef7365143c35ec8c0e
Kaspar Kallas <kaspar@superfluid.finance>
2023-10-24 12:30:09 +0000

    add docs with nextra

---
+++ docs/theme.config.tsx
@@ -0,0 +1,18 @@
+import { DocsThemeConfig } from "nextra-theme-docs";
+import React from "react";
+
+const config: DocsThemeConfig = {
+  logo: <span>Superfluid Checkout Widget</span>,
+  project: {
+    link: "https://github.com/superfluid-finance/widget/",
+  },
+  chat: {
+    link: "https://discord.superfluid.finance/",
+  },
+  docsRepositoryBase: "https://github.com/superfluid-finance/widget/",
+  footer: {
+    text: "Superfluid Checkout Widget",
+  },
+};
+
+export default config;
