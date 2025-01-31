2fe70fb3a575ffad750813dbfc1043cac32654d3
Teodora Jovcheska <jovcheskateodora@gmail.com>
2025-01-31 13:01:55 +0100

    Rerun

--- tests/specs/widgetMetamaskTokenTransactions.spec.ts
+++ tests/specs/widgetMetamaskTokenTransactions.spec.ts
@@ -20,7 +20,7 @@ test.beforeEach(async ({ page }: { page: Page }) => {
 });
 
 test.describe("Token transfer and approval test cases", () => {
-  test.skip("Approving and wrapping tokens", async ({
+  test("Approving and wrapping tokens", async ({
     page,
     metamask,
   }: {
