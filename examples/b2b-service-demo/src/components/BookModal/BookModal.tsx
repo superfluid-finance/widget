22846a53c47dfb6b09e4e79f866356c08c2f1af6
Teodora Jovcheska <jovcheskateodora@gmail.com>
2025-02-07 13:03:58 +0000

    Trigger tests

--- examples/b2b-service-demo/src/components/BookModal/BookModal.tsx
+++ examples/b2b-service-demo/src/components/BookModal/BookModal.tsx
@@ -30,7 +30,7 @@ const BookModal: FC<ModalProps> = ({ show, onClose }) => {
             href="https://use.superfluid.finance/subscriptions"
             target="_blank"
           >
-            Book a Demo Teodora
+            Book a Demo
           </a>
         </div>
       </div>
